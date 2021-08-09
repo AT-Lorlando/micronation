a58e121322279f26a2357ac44706bbb8445b2e1d
spicytelescope <jbeziaud@gmail.com>
2021-08-10 00:44:44 +0200

    fix(AppInputFrame): removing the molecule LabeldInputFrame and implementing native addon with fieldset

---
+++ front/components/atoms/AppCheckbox.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="prose-roboto">
+    <input
+      type="checkbox"
+      id="checkbox"
+      :checked="checked"
+      @change="$emit('change', $event.target.checked)"
+    />
+    <label class="text-grey-dark body-1">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppCheckbox",
+  props: {
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      required: true,
+      type: String,
+      default: undefined,
+    },
+  },
+};
+</script>
+
+<style scoped lang="postcss">
+/*Styling checkbox with a workaround as it's not a html element meant to be styled */
+
+#checkbox {
+  @apply: border border-primary-base;
+}
+</style>
